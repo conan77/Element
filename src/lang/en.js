b60c954db094446f5a20ba0dd04a3c5553cd1815
panfeng <panfeng77@126.com>
2020-08-29 19:24:16 +0800

    初始化

---
+++ src/lang/en.js
@@ -0,0 +1,23 @@
+export const m = {
+  login: {
+    introduction: '欢迎使用报表平台',
+    name_holder: '用户名',
+    password_holder: '密码',
+    store_tip: '请输入门店号',
+    name_tip: '请输入用户名',
+    password_tip: '请输入密码',
+    remember: '记住我',
+    forget: '忘记密码',
+    info: '',
+    button: '登录'
+  },
+  topbar: {
+    sayHi: '你好',
+    logout: '退出'
+  },
+  menu: {
+    Profile: '营业概况',
+    cube: 'Cube',
+    authority: 'Authority'
+  }
+}
---
+++ src/lang/zh.js
@@ -0,0 +1,24 @@
+export const m = {
+  login: {
+    introduction: '欢迎使用菜么么报表平台系统',
+    store_holder: '门店号',
+    name_holder: '用户名',
+    password_holder: '密码',
+    store_tip: '请输入门店号',
+    name_tip: '请输入用户名',
+    password_tip: '请输入密码',
+    remember: '记住我',
+    forget: '忘记密码',
+    info: '',
+    button: '登 录'
+  },
+  topbar: {
+    sayHi: '嗨',
+    logout: '退出'
+  },
+  menu: {
+    Profile: '营业概况',
+    cube: '魔幻立方',
+    authority: '权限测试'
+  }
+}
